fec64c35146297745a92a5fe5f84b5e62f5d6127
razorlace13 <62058757+razorlace13@users.noreply.github.com>
2021-10-14 16:53:11 +0100

    index of backend

--- web/yii-application/backend/views/site/index.php
+++ web/yii-application/backend/views/site/index.php
@@ -2,52 +2,29 @@
 
 /* @var $this yii\web\View */
 
+use backend\assets\AppAsset;
+use yii\bootstrap4\Html;
+
 $this->title = 'My Yii Application';
+AppAsset::register($this);
 ?>
+
 <div class="site-index">
+    <div style="background: url(https://images.wallpaperscraft.com/image/single/coffee_mug_chair_124443_1920x1080.jpg)" class="page-holder bg-cover">
+        <header class="text-center text-white py-5">
+            <h1 class="display-4 font-weight-bold mb-4">Home Page</h1>
 
-    <div class="jumbotron text-center bg-transparent">
-        <h1 class="display-4">Congratulations!</h1>
+        </header>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
+        <div class="body-content center">
+            <?= Html::a('User', ['user/index'], ['class' => 'button-1']) ?>
 
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+            <?= Html::a('label', ['/controller/action'], ['class' => 'button-2']) ?>
         </div>
+        <div class="body-content center">
+            <?= Html::a('label', ['/controller/action'], ['class' => 'button-2']) ?>
 
+            <?= Html::a('label', ['/controller/action'], ['class' => 'button-1']) ?>
+        </div>
     </div>
 </div>

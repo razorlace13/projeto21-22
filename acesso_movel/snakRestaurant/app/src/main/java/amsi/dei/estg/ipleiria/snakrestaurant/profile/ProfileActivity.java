ebf4ea14405d8cfc29f1d853d889d8db6321ec12
razorlace13 <62058757+razorlace13@users.noreply.github.com>
2021-11-01 15:40:07 +0000

    android profile
    the start

---
+++ acesso_movel/snakRestaurant/app/src/main/java/amsi/dei/estg/ipleiria/snakrestaurant/profile/ProfileActivity.java
@@ -0,0 +1,16 @@
+package amsi.dei.estg.ipleiria.snakrestaurant.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import amsi.dei.estg.ipleiria.snakrestaurant.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
